b143e2c7298104b268aa75c3f398306cb7cc2db5
am2990 <apurv.mehra@gmail.com>
2014-04-23 15:48:22 +0530

    -seerver with multikey press

--- RemoteServer/RemoteServer/mainForm.Designer.cs
+++ RemoteServer/RemoteServer/mainForm.Designer.cs
@@ -36,10 +36,9 @@
             // 
             // serverStart_btn
             // 
-            this.serverStart_btn.Location = new System.Drawing.Point(13, 37);
-            this.serverStart_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.serverStart_btn.Location = new System.Drawing.Point(10, 30);
             this.serverStart_btn.Name = "serverStart_btn";
-            this.serverStart_btn.Size = new System.Drawing.Size(365, 124);
+            this.serverStart_btn.Size = new System.Drawing.Size(274, 101);
             this.serverStart_btn.TabIndex = 0;
             this.serverStart_btn.Text = "Start Server";
             this.serverStart_btn.UseVisualStyleBackColor = true;
@@ -49,18 +48,18 @@
             // 
             this.ipAddress_label.AutoSize = true;
             this.ipAddress_label.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ipAddress_label.Location = new System.Drawing.Point(80, 180);
-            this.ipAddress_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ipAddress_label.Location = new System.Drawing.Point(60, 146);
             this.ipAddress_label.Name = "ipAddress_label";
-            this.ipAddress_label.Size = new System.Drawing.Size(114, 17);
+            this.ipAddress_label.Size = new System.Drawing.Size(88, 13);
             this.ipAddress_label.TabIndex = 1;
             this.ipAddress_label.Text = "Not Connected...";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 236);
+            this.button1.Location = new System.Drawing.Point(223, 192);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 2;
             this.button1.Text = "EXIT";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,22 +68,22 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 239);
+            this.label1.Location = new System.Drawing.Point(8, 194);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(284, 17);
+            this.label1.Size = new System.Drawing.Size(215, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Please close the app using the EXIT button:";
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 281);
+            this.ClientSize = new System.Drawing.Size(293, 228);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ipAddress_label);
             this.Controls.Add(this.serverStart_btn);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "mainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Server";
--- RemoteServer/RemoteServer/mainForm.cs
+++ RemoteServer/RemoteServer/mainForm.cs
@@ -11,12 +11,18 @@ using WindowsInput;
 using System.Net;
 using System.IO;
 using System.Net.Sockets;
+using InputManager;
 
 namespace RemoteServer
 {
     public partial class mainForm : Form
     {
         private int modifiedStrokeFlag = 0;
+        private bool setval = false;
+        float _roll = 0;
+        float _pitch = 0;
+        float _yaw = 0;
+
         public mainForm()
         {
             InitializeComponent();
@@ -40,7 +46,7 @@ namespace RemoteServer
             IPAddress[] ip = hostname.AddressList;
             IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
                        
-            while (conStart)
+             while (conStart)
             {
                 data = newsock.Receive(ref sender);
                 string str = Encoding.ASCII.GetString(data, 0, data.Length);
@@ -49,26 +55,63 @@ namespace RemoteServer
                 {
                     
                     int click = int.Parse(str.Split(':')[0]);
-                    if (click > 2)
+                    
+                    if (click > 99)
                     {
                         string keys = str.Split(':')[1];
                         processModifiedStroke(click, keys);
                         continue;
                     }
                     int key = int.Parse(str.Split(':')[1]);
+                    if (click == 2)
+                    {
+                        processGameKey(click, key);
+                        continue;
+                    }
+                    if (click == 8 || click == 9)
+                    {
+                        processStaticKey(click, key);
+                        continue;
+                    }
                     processKey(click,key);
                     continue;
                 }
                 else if (count == 3)
                 {
-                    float x = float.Parse(str.Split(':')[0]);
-                    float y = float.Parse(str.Split(':')[1]);
-                    float z = float.Parse(str.Split(':')[0]);
-                    //Console.WriteLine(str + "|X:" + x + "|Y:" + y + "|Z: " + z);
+                    float roll = float.Parse(str.Split(':')[0]);
+                    float pitch = float.Parse(str.Split(':')[1]);
+                    float yaw = float.Parse(str.Split(':')[2]);
+                    Console.WriteLine(str + "|roll:" + roll + "|pitch:" + pitch + "|yaw: " + yaw);
+                    processValues(pitch, roll, yaw);
+
+
                 }
 
             }
 
+        }
+
+        private void processStaticKey(int click, int key)
+        {
+            if (click == 9)
+            {
+                if (!InputSimulator.IsKeyDown((VirtualKeyCode)key))
+                {
+                    InputSimulator.SimulateKeyDown((VirtualKeyCode)key);
+                    System.Console.WriteLine("Button Down {0}", key);
+
+                }
+
+            }
+            else
+            {
+                if (InputSimulator.IsKeyDown((VirtualKeyCode)key))
+                {
+                    InputSimulator.SimulateKeyUp((VirtualKeyCode)key);
+                    System.Console.WriteLine("Button Up {0}", key);
+                }
+
+            }
         }
 
         private void processModifiedStroke(int click, string keys)
@@ -85,13 +128,16 @@ namespace RemoteServer
 
             
         }
-
-        private void processKey(int click,int key){
+        
+        private void processKey(int click,int key) {
             if (click == 1)
             {
                 if (!InputSimulator.IsKeyDown((VirtualKeyCode)key))
                 {
-                    InputSimulator.SimulateKeyDown((VirtualKeyCode)key);
+                    //InputSimulator.SimulateKeyDown((VirtualKeyCode)key);
+                    System.Console.WriteLine("Button Down {0}", key);
+                    Keyboard.KeyDown((Keys)key);
+
                 }
 
             }
@@ -99,8 +145,159 @@ namespace RemoteServer
             {
                 if (InputSimulator.IsKeyDown((VirtualKeyCode)key))
                 {
-                    InputSimulator.SimulateKeyUp((VirtualKeyCode)key);
+                    //InputSimulator.SimulateKeyUp((VirtualKeyCode)key);
+                    System.Console.WriteLine("Button Up {0}", key);
+                   Keyboard.KeyUp((Keys)key);
                 }
+
+            }
+        }
+
+        private void processValues(float pitch, float roll, float yaw)
+        {
+
+            string data;
+
+            if (roll > 20)
+            {
+                //right arrow
+                //data = "1:" + "39";
+                //prev = "39";
+                //int c = 0;
+                System.Diagnostics.Debug.WriteLine("Right Arrow {0}", pitch);
+
+                //while (c < 10)
+                // {
+                //data = "1:32";
+
+                processGameKey(1, 39);
+                //c++;
+                // }
+            }
+            else if (roll < -20)
+            {
+                //left arrow
+                //data = "1:37";
+                //prev = "37";
+                //int c = 0;
+                //System.Diagnostics.Debug.WriteLine("Left Arrow {0}", delta_pitch);
+                //while (c < 10)
+                //{
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+                processGameKey(1, 37);
+                  //  c++;
+                //}
+            }
+            else if (roll > -20 && roll < 20)
+            {
+                processGameKey(0, 37);
+                processGameKey(0, 39);
+            }
+            if (pitch < -15)
+            {
+                //down arrow
+                //data = "1:40";
+                //prev = "40";
+               // int c = 0;
+                System.Diagnostics.Debug.WriteLine("Down Arrow {0}", roll);
+               // while (c < 10)
+               // {
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+                    //sendData.Send(serverName, portNumber, data);
+                    //c++;
+                //}
+                processGameKey(1,40);
+            }
+            else if (pitch > 10)
+            {
+                //up arrow
+               // data = "1:38";
+              // // prev = "38";
+              //  int c = 0;
+                System.Diagnostics.Debug.WriteLine("Up Arrow {0}", pitch);
+               // while (c < 10)
+              //  {
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+               //     sendData.Send(serverName, portNumber, data);
+                //    c++;
+               // }
+                processGameKey(1,38);
+            }
+            else if( pitch >= -15 && pitch<= 10 )
+            {
+                processGameKey(0, 38);
+                processGameKey(0, 40);
+            }
+            /*
+            
+            else if (delta_yaw < -20)
+            {
+                //E arrow
+                data = "1:69";
+                prev = "69";
+                int c = 0;
+                System.Diagnostics.Debug.WriteLine("E {0}", delta_yaw);
+                while (c < 10)
+                {
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+                    sendData.Send(serverName, portNumber, data);
+                    c++;
+                }
+            }
+            else if (delta_yaw > 20)
+            {
+                //Q arrow
+                data = "1:81";
+                prev = "81";
+                int c = 0;
+                System.Diagnostics.Debug.WriteLine("Q {0}", delta_yaw);
+                while (c < 10)
+                {
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+                    sendData.Send(serverName, portNumber, data);
+                    c++;
+                }
+            }
+            else
+            {
+                data = "0:" + prev;
+                int c = 0;
+                System.Diagnostics.Debug.WriteLine("KeyUp {0}", data);
+                while (c < 10)
+                {
+                    //System.Diagnostics.Debug.WriteLine("Fired down {0}", c++);
+                    //data = "1:32";
+                    sendData.Send(serverName, portNumber, data);
+                    c++;
+                }
+            }*/
+
+
+        }
+
+        private void processGameKey(int click, int key)
+        {
+            if (click == 1)
+            {
+                
+                    //InputSimulator.SimulateKeyDown((VirtualKeyCode)key);
+                    Keyboard.KeyDown((Keys)key);
+                    //System.Console.WriteLine("Key Down {0}", key);
+                
+
+            }
+            else
+            {
+                
+                    //InputSimulator.SimulateKeyUp((VirtualKeyCode)key);
+                    Keyboard.KeyUp((Keys)key);
+                    //System.Console.WriteLine("Key Up {0}", key);
+                
 
             }
         }
